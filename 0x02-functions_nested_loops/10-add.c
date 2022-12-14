0b4626d7976e0db263861f01266621873c755ff4
Constancemok4 <constancemok4@gmail.com>
2022-12-14 10:39:46 -0800

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - Entry point
+ *
+ * @a: value to be added together
+ * @b: second value to be added
+ *
+ * Return: Always 0
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
