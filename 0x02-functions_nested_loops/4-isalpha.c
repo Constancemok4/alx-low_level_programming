de82a39e502dacc131e66c4064dc35920d39f1e1
Constancemok4 <constancemok4@gmail.com>
2022-12-14 04:16:57 -0800

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isalpha - Return 1 if c is a letter. lowercase or uppercase
+ * @c: character to be checked
+ * Return: 0 or 1
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= d)
+		return (1);
+	else
+		return (0);
+}
