f58c44742d77a0ca9f508c6249a300cf9ea2b46a
Constancemok4 <constancemok4@gmail.com>
2022-12-14 09:52:12 -0800

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _abs - computes the value of an integer
+ * #n: the number to be checked
+ *
+ * Return: returns an unsigneed int value
+ */
+
+int _abs(int n)
+{
+	if (n > 0)
+	{
+		return (n);
+	}
+
+	else
+	{
+		n *= -1;
+		return (n);
+	}
+}
