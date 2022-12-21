64e3b7a8dc37edf257b4bbcac1cc59dbee7815cc
Constancemok4 <constancemok4@gmail.com>
2022-12-21 10:23:09 -0800

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+/**
+ * 0-reset_to_98.c - takes a pointer to an int parameter and update, the value it points to 98
+ * 
+ * @n: int parameters
+ * 
+ * Return: Nothing
+ */
+void 0-reset_to_98.c(int *n)
+{
+*n = 98;
+}
