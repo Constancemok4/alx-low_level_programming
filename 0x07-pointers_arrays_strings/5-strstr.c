13077311309d72344955852440d16e60df3c2624
Constancemok4 <constancemok4@gmail.com>
2023-01-03 19:05:18 -0800

    7-print_chessboard.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * _strstr - locates a substring.
  * @haystack: searches a string
@@ -7,27 +6,9 @@
  *
  * Return: if the subdtring is not located - NULL.
  */
-
 char *_strstr(char *haystack, char *needle)
 {
 	int index;
 
 	if (*needle == 0)
-		return (haystack);
-	while (*haystack)
-	{
-		index = 0;
-		if (haystack[index] == needle[index])
-		{
-			do {
-				if (needle[index + 1] == '\0')
-					return (haystack);
-				index++;
-			}
-			while (haystack[index] == needle[index]);
-		}	
-		haystack++;
-	}
-	return ('\0');
-}
-				
+		return (
--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,20 +1,19 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
  * print_chessboard - prints chessboard
  * @a: matrix
+ * Return: Always 0
  */
-
 void print_chessboard(char (*a)[8])
 {
-	int i, j;
+	int i;
+	int j;
 
 	for (i = 0; i < 8; i++)
 	{
 		for (j = 0; j < 8; j++)
-		{
-			_putchar(*(*(i + a) + j));
-		}
-	_putchar('\n');
+			putchar(a[i][j]);
+		putchar('\n');
 	}
 }
