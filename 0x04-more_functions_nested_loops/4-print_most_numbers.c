bdda0d72f46bb8410aa6b26d6d077014c8781983
Constancemok4 <constancemok4@gmail.com>
2023-02-17 03:49:03 -0800

    task4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * print_most_numbers - prints the numbers from 0 to 9
- * Return: Always 0
+ * print_most_numbers - print numbers from 0 to 9
+ * Return: 0
  */
 void print_most_numbers(void)
 {
